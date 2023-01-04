87c6f0181560b2c783be4fcc9a4aa8a53d0a9f7f
qkrwjdan <madogisa12@naver.com>
2023-01-05 01:04:48 +0900

    [feat] logger 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,48 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoresModule } from './stores/stores.module';
-
+import * as winston from 'winston';
+import * as WinstonDaily from 'winston-daily-rotate-file';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import { NestModule } from '@nestjs/common';
+import { MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 @Module({
-  imports: [StoresModule],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike(),
+          ),
+        }),
+        new WinstonDaily({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike(),
+          ),
+          level: 'debug',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          filename: 'custom-logs-all/%DATE%.log',
+          maxSize: '20m',
+          maxFiles: '28d',
+        }),
+      ],
+    }),
+    StoresModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl, body } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+      if (method !== 'GET') {
+        this.logger.log(body);
+      }
+    });
+
+    next();
+  }
+}
